9977f9aa7906fa2e2415c43c1d5ac8e6df2e9197
milo <loli.bachmann88@gmail.com>
2026-08-27 11:58:29 -0300

    clase 27-08

--- orm/practica.py
+++ orm/practica.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Float, Boolean, create_engine, select, func
+from sqlalchemy import Column, Integer, String, Float, Boolean, create_engine, select, func, or_
 from sqlalchemy.orm import DeclarativeBase, Session, sessionmaker
 
 #comando para correr con orm
@@ -40,7 +40,7 @@ with Session() as session:
     productos_menor_500 = session.query(Producto) \
                       .filter(Producto.precio < 500) \
                       .all()
-
+    
     print ("Productos con precio menor a $500: ")
     for p in productos_menor_500:
         print(p.nombre, p.precio)  # ← atributos reales, no índices
@@ -57,6 +57,7 @@ productos_nuevos= [
     Producto(nombre="Hub USB-C", precio=5400, stock=6, categoria="accesorios", activo= False),
     Producto(nombre="Cable HDMI", precio=1800, stock=50, categoria="accesorios", activo= True),
     Producto(nombre="Webcam full HD", precio=9800, stock=12, categoria="perifericos", activo= True),
+    Producto(nombre="Silla gamer", precio= None, stock= 3, categoria= None, activo= False) 
 ]
 
 with Session() as session:
@@ -64,7 +65,11 @@ with Session() as session:
     session.commit()
     cantidad = session.query(Producto).count()
 
+    print(" ")
+    print("Ejercicio 2 ")
+    print(" ")
     print(cantidad)
+    print("Tengo 5 más pq son los que tenía antes")
 
 #----------------------------------3-------------------------------------------------
 with Session() as session:
@@ -72,6 +77,9 @@ with Session() as session:
         .filter(Producto.categoria == "perifericos") \
         .all()
 
+    print(" ")
+    print("Ejercicio 3")
+    print(" ")
     print("Productos de la categoría periféricos:")
     for p in productos_perifericos:
         print(p.nombre, p.precio)
@@ -82,7 +90,9 @@ with Session() as session:
                       .filter(Producto.precio >10000) \
                       .order_by(Producto.precio.desc())\
                       .all()
-
+    print(" ")
+    print("Ejercicio 4")
+    print(" ")
     print ("Productos con precio menor a $10000 ordenados: ")
     for p in productos_mayor_10000_ordenados:
         print(p.nombre, p.precio)  # ← atributos reales, no índices
@@ -93,7 +103,9 @@ with Session() as session:
                       .filter(Producto.stock <= 12) \
                       .filter(Producto.activo == True)\
                       .all()
-
+    print(" ")
+    print("Ejercicio 5")
+    print(" ")
     print ("Productos con stock menor o igual a 12 activos ")
     for p in productos_stock12_activos:
         print(p.nombre, p.stock)
@@ -103,7 +115,9 @@ with Session() as session:
                       .filter(Producto.precio > 5000) \
                       .filter(Producto.precio < 20000)\
                       .all()
-
+    print(" ")
+    print("Ejercicio 6")
+    print(" ")
     print ("Productos entre 5000 y 20000 ")
     for p in productos_entre_5y20:
         print(p.nombre)
@@ -112,7 +126,9 @@ with Session() as session:
     producto_caro = session.query(Producto) \
                       .order_by(Producto.precio.desc()) \
                       .first()
-
+    print(" ")
+    print("Ejercicio 7")
+    print(" ")
     print ("Producto mas caro")
     print(producto_caro.nombre)
 
@@ -122,9 +138,12 @@ with Session() as session:
                         .filter(Producto.activo == False)\
                         .all()
 
+    print(" ")
+    print("Ejercicio 8")
+    print(" ")
     print ("Productos inactivos")
 #    for p in producto_inactivo:
-#        print(producto_inactivo.nombre)
+#       print(producto_inactivo.nombre)
 
 #------------------------extra---------------------------------------
 with Session() as session:
@@ -141,7 +160,112 @@ with Session() as session:
     productos_usb = session.query(Producto)\
                         .filter(Producto.nombre.contains("USB"))\
                         .all()
-
+    print(" ")
+    print("Extra")
+    print(" ")
     print ("Productos con USB")
     for p in productos_usb:
         print(p.nombre)
+
+#-----------------9------------------
+with Session() as session:
+    p_audio_componentes = session.query(Producto) \
+                      .where (or_(Producto.categoria == "audio",Producto.categoria == "componentes")) \
+                      .all()
+
+    print(" ")
+    print("Ejercicio 9")
+    print(" ")
+    print ("Productos de audio o componentes")
+    for p in p_audio_componentes:
+        print(p.nombre)
+#--------------10----------------------
+with Session() as session:
+    productos_a = session.query(Producto) \
+                      .filter(Producto.nombre.contains("a")) \
+                      .all()
+
+    print(" ")
+    print("Ejercicio 10")
+    print(" ")
+    print ("Productos con a")
+    for p in productos_a:
+        print(p.nombre)
+    print(f"Hay {len(productos_a)} productos")
+
+#--------------11----------------------
+with Session() as session:
+    productos_m = session.query(Producto) \
+                      .filter(Producto.nombre.startswith("M")) \
+                      .all()
+
+    print("\nEjercicio 11\n")
+    print ("Productos que empiezan con M")
+    for p in productos_m:
+        print(p.nombre)
+
+#--------------12----------------------
+with Session() as session:
+    productos_none = session.query(Producto) \
+                      .filter(Producto.categoria==None) \
+                      .all()
+
+    print("\nEjercicio 12\n")
+    print ("Productos con categoria none")
+    for p in productos_none:
+        print(p.nombre)
+
+#si filtras por precio no pasa nada
+
+#--------------13----------------------
+with Session() as session:
+    usb = session.query(Producto)\
+        .filter(Producto.nombre == "Hub USB-C").first()
+    idusb= usb.id
+    producto_usb = session.get(Producto, idusb) 
+    Producto.stock= 20
+    Producto.activo= True
+    session.commit()
+                      
+
+    print("\nEjercicio 13\n")
+    print ("Hub USB-C actualizado:")
+    print(producto_usb.nombre, producto_usb.stock, producto_usb.activo)
+#-----------------14----------------------
+with Session() as session:
+    perifericos= session.query(Producto)\
+                      .filter(Producto.categoria=="perifericos")\
+                      .all()
+
+    print("\nEjercicio 14\n")
+    for p in perifericos:
+        p.precio= p.precio + p.precio*0.1
+        session.commit()
+        print(p.nombre, p.precio)
+#el teclado vale 9350
+
+#------------15-----------------
+with Session() as session: 
+    hdmi = session.query(Producto)\
+            .filter(Producto.nombre == "Cable HDMI").first()
+    idhdmi= hdmi.id
+    producto_hdmi = session.get(Producto, idhdmi) 
+    session.delete(producto_hdmi)
+    session.commit()
+    cant= session.query(Producto).count()
+print("\nEjercicio 15\n")
+print(cant)
+print("Tengo 5 mas pq son los que tenia inicialmente")
+
+#--------------16-------------------------
+with Session() as session:
+    inactivos_menos200=session.query(Producto) \
+                      .where (or_(Producto.precio < 200,Producto.activo == False)) \
+                      .all()
+    for p in inactivos_menos200:
+        session.delete(p)
+    session.commit()
+    cantt= session.query(Producto).count()
+    print("\nEjercicio 16\n")
+    print(cantt)
+#----------------extra-------------------------
